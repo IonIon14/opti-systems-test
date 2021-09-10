13776614ae53e8a6a50bc310691dc3bb01d07959
IonIon14 <ionut.baciu1501@stud.etti.upb.ro>
2021-09-10 18:47:46 +0300

    fix slider and dropdown

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,47 @@ window.addEventListener("resize", () => {
     list.classList.add("hidden");
   }
 });
+
+const dropdown = document.querySelector(".dropdownButton");
+const dropdownList = document.querySelector("#menu-dropdown");
+
+dropdown.addEventListener("mouseover", () => {
+  const dropdownList = document.querySelector("#menu-dropdown");
+  dropdownList.classList.toggle("hidden");
+});
+
+dropdownList.addEventListener("mouseleave", () => {
+  
+  dropdownList.classList.toggle("hidden");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  global_carousel__ctrl();
+});
+
+const slider_one = () => {
+  const one = new Splide("#one", {
+    autoplay: false,
+    pagination:false,
+    keyboard: "focused", //enabling keyboard for focused element
+  }).mount();
+};
+
+const slider_two = () => {
+  const two = new Splide("#two", {
+    autoplay: false,
+    pagination:false,
+    keyboard: "focused", //enabling keyboard for focused element
+  }).mount();
+};
+
+//alternate way of triggering carousels in muiltiple instances
+const global_carousel__ctrl = () => {
+  const elms = document.getElementsByClassName("splide");
+  for (let i = 0; i < elms.length; i++) {
+    new Splide(elms[i], {
+      autoplay: false,
+      keyboard: "focused", //enabling keyboard for focused element
+    }).mount();
+  }
+};
